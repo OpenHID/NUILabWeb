9ed701cb2f1142a4655fe0e1541b792dd02496c7
Santiago B <stonbx@gmail.com>
2018-01-25 19:26:00 -0500

    Created Home Component and added Assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routing Module to redirect pages
+import { AppRoutingModule }     from './app-routing.module';
 
+// Site Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
