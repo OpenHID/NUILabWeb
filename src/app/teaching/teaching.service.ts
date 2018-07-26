9dbae7061d577064cb52eb9399835728f796791f
Santiago B <stonbx@gmail.com>
2018-07-26 09:52:20 -0400

    Added all the classes and updated JSON Obj

--- src/app/teaching/teaching.service.ts
+++ src/app/teaching/teaching.service.ts
@@ -13,7 +13,7 @@ export class TeachService {
                         {
                             "code":"CS457",
                             "name":"Computer Networks and the Internet",
-                            "time":"10:00AM-11:00AM",
+                            "time":"TBA",
                             "description":"This senior level undergraduate course covers principles of data communications, local area networks, communications protocols, TCP/IP, and the Internet.",
                             "isLink":true,
                             "link":"https://www.cs.colostate.edu/~cs457"
@@ -22,12 +22,12 @@ export class TeachService {
                     "Spring2019":
                     [
                         {
-                            "code":"CS457",
-                            "name":"Computer Networks and the Internet",
-                            "time":"10:00AM-11:00AM",
-                            "description":"This senior level undergraduate course covers principles of data communications, local area networks, communications protocols, TCP/IP, and the Internet.",
+                            "code":"CS464",
+                            "name":"Principles of Human-Computer Interaction",
+                            "time":"TBA",
+                            "description":"Human-Computer Interaction teaches the fundamental issues that underlie the creation and evaluation of usable and useful computational artifacts. Over the term, students will learn how to design novel computational artifacts that enable a well-defined user group to achieve specific goals more effectively than via current means.",
                             "isLink":false,
-                            "link":"https://www.cs.colostate.edu/~cs457"
+                            "link":"https://www.cs.colostate.edu/~cs464/"
                         }
                     ]
                 }
